ae0897cb79b06052dd7f979dde89edd09cd0cdf7
imdhruv99 <dpdhruvprajapati@gmail.com>
2020-09-13 21:51:04 +0530

    Django Vue ToDo App

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+
+</style>
---
+++ server/task/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Task
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+
+        model = Task
+
+        fields = (
+            'id',
+            'description',
+            'status'
+        )
